4c2c865eac44bc8a85be49cb90950c19922327a7
HOBIRON <yudainepnep@icloud.com>
2021-01-21 18:52:30 +0900

    feat: ポップアップからAPIを叩く機能を実装

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,20 +1,59 @@
-var elementsInsideBody = [...document.body.getElementsByTagName("*")];
+$(function () {
+  $("#result").text("通信中...");
+  // Ajax通信を開始
+  $.ajax({
+    url: "https://word-collection-api.herokuapp.com/api-auth/",
+    type: "post",
+    dataType: "json",
+    // フォーム要素の内容をハッシュ形式に変換
+    data: { username: "test", password: "1234" },
+    timeout: 5000,
+  }).then(
+    function (data) {
+      // 通信成功時の処理を記述
+      console.log("success: ");
 
-function findAndReplace() {
-  elementsInsideBody.forEach((element) => {
-    element.childNodes.forEach((child) => {
-      if (child.nodeType === 3) {
-        replaceText(child);
-      }
-    });
-  });
+      console.log(data);
+    },
+
+    function (error) {
+      // 通信失敗時の処理を記述
+      console.log("error: ");
+      console.log(error);
+    }
+  );
+});
+
+// const login = (e) => {
+//   e.preventDefault();
+//   setIsloading(true);
+
+//   let form_data = new FormData();
+//   form_data.append("username", userName);
+//   form_data.append("password", password);
+//   axios
+//     .post(apiServer + "api-auth/", form_data, {
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     })
+//     .then((response) => {
+//       const jwt = response.data.token;
+//       localStorage.setItem("jwt", jwt);
+//       history.push("/mypage");
+//     })
+//     .catch((error) => {
+//       setIsloading(false);
+//       setIsError(true);
+//       console.log(error);
+//     });
+// };
+
+console.log("Hello World");
+let token = localStorage.getItem("jwt");
+if (token) {
+  console.log(token);
+} else {
+  console.log("No: ");
+  console.log(token);
 }
-
-function replaceText(node) {
-  let value = node.nodeValue;
-  value = value.replace(/リ/gi, "ソ");
-  value = value.replace(/ン/gi, "ソ");
-  node.nodeValue = value;
-}
-
-window.onload = findAndReplace();
